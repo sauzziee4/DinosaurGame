a47870620ebbf34bfd52f8aa83520d7396438506
sauzziee4 <sarahupton04@icloud.com>
2025-07-04 14:12:40 +0200

    Added Camera Movement and Animation Clips for horizontal and vertical

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,16 +1,29 @@
+using Unity.Collections;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    [SerializeField] public float speed = 2f;
+    Vector2 motionVector;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    void FixedUpdate()
+    {
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        rigidbody2d.linearVelocity = motionVector * speed; 
     }
 }
